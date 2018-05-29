836d89b0b93f6e52567dc0591871fd48358c68fd
Terry Woodruff <terry_woodruff@hotmail.com>
2018-05-29 19:12:50 +0100

    default uses the azure service at https://reacttccc.azurewebsites.net

--- ReactTCCCLogic/Constants.cs
+++ ReactTCCCLogic/Constants.cs
@@ -5,8 +5,8 @@ namespace ReactFrameworkLogic
     public static class Constants
     {
         // Replace strings with your Azure Mobile App endpoint.
-        //public static string ApplicationURL = @"https://reacttccc.azurewebsites.net";
-        public static string ApplicationURL = @"http://localhost:51039";
+        public static string ApplicationURL = @"https://reacttccc.azurewebsites.net";
+        //public static string ApplicationURL = @"http://localhost:51039";
     }
 }
 
